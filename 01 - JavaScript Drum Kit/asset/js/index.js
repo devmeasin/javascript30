9d95483aa465531ac2a99ee478e8fcb99ac8a724
easinwebpro <easinislam299@gmail.com>
2021-02-13 08:18:33 +0600

    JavaScript Drum Project

---
+++ 01 - JavaScript Drum Kit/asset/js/index.js
@@ -0,0 +1,21 @@
+function playmusic(event) {
+  const music = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+  music.currentTime = 0;
+  music.play();
+  key.classList.add("playing");
+  console.log(key);
+}
+
+function removeClass(event) {
+  if (event.propertyName !== "transform") return;
+  this.classList.remove("playing");
+  //   console.log(this);
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => {
+  key.addEventListener("transitionend", removeClass);
+});
+
+window.addEventListener("keydown", playmusic);
